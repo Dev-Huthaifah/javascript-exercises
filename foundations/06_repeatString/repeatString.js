404f525e895ec0ab3acfaff31dce630ad7402cd9
Huthaifah <123631430+Dev-Huthaifah@users.noreply.github.com>
2026-03-19 04:56:02 +0000

    repeatString - Solved.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(a, b) {
+  if ( b >= 0 ) {
+  let result = "";
+  for (let i = 1; i <= b; i++) {
+    result += a;
+}
+  return result;
+} else {
+  return "ERROR";
+}
 };
 
 // Do not edit below this line
