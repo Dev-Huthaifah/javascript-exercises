5603fb22c62b72b84ffccea41959917391a844c9
Huthaifah68 <123631430+Dev-Huthaifah@users.noreply.github.com>
2026-03-24 07:29:30 +0300

    tempConversion - Solved.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(deg) {
+  let result = (deg - 32) * 5/9;
+  let rounded = Math.round(result * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(deg) {
+  let result = (deg * 9/5 + 32);
+  let rounded = Math.round(result * 10) /10;
+  return rounded;
 };
 
 // Do not edit below this line
