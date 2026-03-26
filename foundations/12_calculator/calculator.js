e13106380eae77885a35dd05b90cc61e75f3960a
Huthaifah <123631430+Dev-Huthaifah@users.noreply.github.com>
2026-03-26 10:03:55 +0300

    Calculator - Solved.

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,53 @@
-const add = function() {
-	
+const add = function (a, b) {
+  if (typeof a != 'number' || typeof b != 'number') {
+    return 'ERROR';
+  } else {
+    return a + b;
+  }
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  if (typeof a != 'number' || typeof b != 'number') {
+    return 'ERROR';
+  } else {
+    return a - b;
+  }
 };
 
-const sum = function() {
-	
+const sum = function (numbers) {
+  let sumResult = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sumResult += numbers[i];
+  }
+  return Number(sumResult);
 };
 
-const multiply = function() {
-
+const multiply = function (numbers) {
+  let product = 1;
+  for (let i = 0; i < numbers.length; i++) {
+    product *= numbers[i];
+  }
+  return Number(product);
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  if (typeof a != 'number' || typeof b != 'number') {
+    return 'ERROR';
+  } else {
+    return Math.pow(a, b);
+  }
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (typeof n != 'number') {
+    return 'ERROR';
+  } else {
+    if (n === 0) {
+      return 1;
+    } else {
+      return n * factorial(n - 1);
+    }
+  }
 };
 
 // Do not edit below this line
